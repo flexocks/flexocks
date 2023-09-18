abd7d4d2263d01a6031d3851285ea936dd4257c9
Rodrigo <rodrigo@Rodrigos-MBP.fritz.box>
2023-09-18 16:57:36 +0200

    Commit inicial

--- Flexocks/installbrew.sh
+++ Flexocks/installbrew.sh
@@ -2,33 +2,49 @@
 
 check_installed() {
     local cmd=$1
-    local check_paths=(/usr/local/bin /usr/bin /bin /usr/sbin /sbin)
+    local check_paths=(/usr/local/bin /usr/bin /bin /usr/sbin /sbin /opt/homebrew/bin)
     for path in "${check_paths[@]}"; do
         if [[ -x "$path/$cmd" ]]; then
+            echo "$path/$cmd"
             return 0
         fi
     done
     return 1
 }
 
-# Instalar Homebrew si no está presente
-check_installed brew
+add_to_path() {
+    local path_to_add=$1
+
+    local shell_profile
+    if [[ "$SHELL" == "/bin/zsh" ]]; then
+        shell_profile="$HOME/.zshrc"
+    else
+        shell_profile="$HOME/.bash_profile"
+    fi
+
+    echo "Añadiendo $path_to_add al $shell_profile..."
+    echo "export PATH=\"$path_to_add:\$PATH\"" >> "$shell_profile"
+    source "$shell_profile"
+}
+
+brew_path=$(check_installed brew)
 if [[ $? -ne 0 ]]; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-    # Si la instalación de Homebrew falla, terminamos el script
     if [[ $? -ne 0 ]]; then
         echo "Ocurrió un error al instalar Homebrew. Finalizando..."
         exit 1
     fi
 
-    # Actualizamos Homebrew para asegurarnos de que tengamos las fórmulas más recientes
     brew update
 else
-    echo "Homebrew ya está instalado. Saltando su instalación..."
+    echo "Homebrew ya está instalado en $brew_path. Saltando su instalación..."
+
+    if [[ ! "$PATH" =~ "$brew_path" ]]; then
+        add_to_path "$(dirname "$brew_path")"
+    fi
 fi
 
-# Comprobar e instalar autossh si no está presente
 check_installed autossh
 if [[ $? -ne 0 ]]; then
     echo "autossh no está presente. Instalando..."
@@ -37,7 +53,6 @@ else
     echo "autossh ya está instalado. Saltando su instalación..."
 fi
 
-# Comprobar e instalar expect si no está presente
 check_installed expect
 if [[ $? -ne 0 ]]; then
     echo "expect no está presente. Instalando..."
@@ -45,3 +60,4 @@ if [[ $? -ne 0 ]]; then
 else
     echo "expect ya está instalado. Saltando su instalación..."
 fi
+
