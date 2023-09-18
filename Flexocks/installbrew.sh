436f481c72bcbde8f2cb15bb564addc79eb56d12
Rodrigo <rodrigo@Rodrigos-MBP.fritz.box>
2023-09-18 16:36:13 +0200

    Commit inicial

--- Flexocks/installbrew.sh
+++ Flexocks/installbrew.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#!/bin/bash
+
+# Ejecutamos directamente la instalación de Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo "export PATH=/opt/homebrew/bin:$PATH" >> ~/.bash_profile
 source ~/.bash_profile
