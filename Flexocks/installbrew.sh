8698eeb1298e8925814c9c467fa17801636300ab
Rodrigo <rodrigo@Rodrigos-MBP.fritz.box>
2023-09-18 16:49:13 +0200

    Commit inicial

--- Flexocks/installbrew.sh
+++ Flexocks/installbrew.sh
@@ -1,12 +1,47 @@
 #!/bin/bash
 
-#!/bin/bash
+check_installed() {
+    local cmd=$1
+    local check_paths=(/usr/local/bin /usr/bin /bin /usr/sbin /sbin)
+    for path in "${check_paths[@]}"; do
+        if [[ -x "$path/$cmd" ]]; then
+            return 0
+        fi
+    done
+    return 1
+}
 
-# Ejecutamos directamente la instalación de Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Instalar Homebrew si no está presente
+check_installed brew
+if [[ $? -ne 0 ]]; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-echo "export PATH=/opt/homebrew/bin:$PATH" >> ~/.bash_profile
-source ~/.bash_profile
+    # Si la instalación de Homebrew falla, terminamos el script
+    if [[ $? -ne 0 ]]; then
+        echo "Ocurrió un error al instalar Homebrew. Finalizando..."
+        exit 1
+    fi
 
-exit 0
+    # Actualizamos Homebrew para asegurarnos de que tengamos las fórmulas más recientes
+    brew update
+else
+    echo "Homebrew ya está instalado. Saltando su instalación..."
+fi
 
+# Comprobar e instalar autossh si no está presente
+check_installed autossh
+if [[ $? -ne 0 ]]; then
+    echo "autossh no está presente. Instalando..."
+    brew install autossh
+else
+    echo "autossh ya está instalado. Saltando su instalación..."
+fi
+
+# Comprobar e instalar expect si no está presente
+check_installed expect
+if [[ $? -ne 0 ]]; then
+    echo "expect no está presente. Instalando..."
+    brew install expect
+else
+    echo "expect ya está instalado. Saltando su instalación..."
+fi
